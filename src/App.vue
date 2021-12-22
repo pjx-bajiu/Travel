bd9e3a0071485072b1e058411bf199203c76d74e
pjx-bajiu <1481877603@qq.com>
2021-12-22 23:26:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- router-view显示的是当前路由地址所对应的内容 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩/主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe600;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader",
+};
+</script>
+
+<style lang="stylus" scoped>
+.header {
+  display: flex;
+  line-height: 0.86rem;
+  background: #00bcd4;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+    text-align: center;
+
+    .back-icon {
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-input {
+    flex: 1;
+    margin-top: 0.12rem;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    padding-left: 0.2rem;
+    margin-left: 0.2rem;
+    background: #fff;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    line-height: 0.86rem;
+    text-align: center;
+
+    .arrow-icon {
+      margin-left: -0.05rem;
+      font-size: 0.24rem;
+    }
+  }
+}
+</style>
