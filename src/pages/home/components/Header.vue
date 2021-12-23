bfad126dde7c14ec381065e66580d41189b32b5a
pjx-bajiu <1481877603@qq.com>
2021-12-23 14:03:20 +0800

    header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -21,10 +21,11 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl'
 .header {
   display: flex;
   line-height: 0.86rem;
-  background: #00bcd4;
+  background: $bgColor;
   color: #fff;
 
   .header-left {
