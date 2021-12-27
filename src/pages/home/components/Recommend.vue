b389ed822041ef01073c0eadcc362ace7b487ce6
pjx-bajiu <1481877603@qq.com>
2021-12-27 15:14:02 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul class="item-margin">
+      <li class="item border-botttom" v-for='item of recommendList' :key='item.id'>
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRcommend",
+  data () {
+      return {
+          recommendList: [{
+          id:'0001',
+          imgUrl: require('../../../assets/styles/images/cat.jpg'),
+          title:'猫咖',
+          desc:'60元畅游2小时,60元畅游2小时,60元畅游2小时.60元畅游2小时'
+      },
+      {
+          id:'0002',
+          imgUrl: require('../../../assets/styles/images/cat.jpg'),
+          title:'猫咖',
+          desc:'60元畅游2小时,60元畅游2小时,60元畅游2小时.60元畅游2小时'
+      },
+      {
+          id:'0003',
+          imgUrl: require('../../../assets/styles/images/cat.jpg'),
+          title:'猫咖',
+          desc:'60元畅游2小时,60元畅游2小时,60元畅游2小时.60元畅游2小时'
+      },
+      {
+          id:'0004',
+          imgUrl: require('../../../assets/styles/images/cat.jpg'),
+          title:'猫咖',
+          desc:'60元畅游2小时,60元畅游2小时,60元畅游2小时.60元畅游2小时'
+      }]
+      }
+  }
+};
+</script>
+
+<style lang='stylus' scope>
+@import '~styles/minxins.styl'
+.title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+.item-margin {
+    margin-top: .1rem;
+    margin-left: .08rem;
+}
+.item {
+    display: flex;
+    height: 1.9rem;
+    &-img {
+        width: 1.7rem;
+        height: 1.7rem;
+        padding: .1rem;
+    }
+    &-info {
+        flex: 1;
+        padding: .1rem;
+        min-width: 0;
+        .item-title {
+            line-height: .54rem;
+            font-size: .32rem;
+            ellipsis();
+        }
+        .item-desc {
+            line-height: .4rem;
+            color: #ccc;
+            ellipsis();
+        }
+        .item-button {
+            line-height: .44rem
+            margin-top: .16rem;
+            background: #ff9300;
+            padding: 0 .2rem;
+            border-radius: .06rem;
+            color: #fff
+        }
+    }
+}
+</style>  
--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -1,130 +1,145 @@
 <template>
   <div class="icons">
-      <swiper>
-      <swiper-slide v-for='(page,index) of pages' :key='index'>
-      <div class="icon" v-for='item of page' :key='item.id'>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img">
-              <img class='icon-img-content' :src="item.imgUrl" />
+            <img class="icon-img-content" :src="item.imgUrl" />
           </div>
-          <p class="icon-desc">{{item.desc}}</p>
-      </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
       </swiper-slide>
-      </swiper>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-    name:'HomeIcons',
-    data() {
-        return {
-            iconList:[{
-                id:'0001',
-                imgUrl: require('../../../assets/styles/images/hotel.png'),
-                desc:'景点门票'
-            },
-            {
-                id:'0002',
-                imgUrl: require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0003',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0004',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0005',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0006',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0007',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0008',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'0009',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'00010',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            {
-                id:'00011',
-                imgUrl:require('../../../assets/styles/images/flight.png'),
-                desc:'滑雪季节'
-            },
-            ]
+  name: "HomeIcons",
+  data() {
+    return {
+      iconList: [
+        {
+          id: "0001",
+          imgUrl: require("../../../assets/styles/images/hotel.png"),
+          desc: "景点门票",
+        },
+        {
+          id: "0002",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0003",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0004",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0005",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0006",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0007",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0008",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "0009",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "00010",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+        {
+          id: "00011",
+          imgUrl: require("../../../assets/styles/images/flight.png"),
+          desc: "滑雪季节",
+        },
+      ],
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
         }
+        pages[page].push(item);
+      });
+      return pages;
     },
-    computed: {
-        pages () {
-            const pages = []
-            this.iconList.forEach((item,index) => {
-                const page = Math.floor(index / 8)
-                if (!pages[page]) {
-                    pages[page] = []
-                }
-                pages[page].push(item)
-            })
-            return pages
-        }
-    }
-}
+  },
+};
 </script>
 
 <style lang='stylus' scope>
-  @import '~styles/varibles.styl'
-  @import '~styles/minxins.styl'
-  .icons >>> .swiper-contsiner
-    height: 0
-    padding-bottom: 50%
-  .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTexColor
-        ellipsis()
+@import '~styles/varibles.styl';
+@import '~styles/minxins.styl';
+
+.icons >>> .swiper-contsiner {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icons {
+  margin-top: 0.1rem;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+
+    .icon-img-content {
+      display: block;
+      margin: 0 auto;
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-height: 0.44rem;
+    text-align: center;
+    color: $darkTexColor;
+    ellipsis();
+  }
+}
 </style>
