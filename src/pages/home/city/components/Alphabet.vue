5401c193676f9bcfed4793bef76de92811ef5ef4
pjx-bajiu <1481877603@qq.com>
2021-12-30 15:36:55 +0800

    list

---
+++ src/pages/home/city/components/Alphabet.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+        <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityAlphabet",
+};
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl';
+.list {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: absolute;
+    right:0;
+    top: 1.58rem;
+    bottom: 0;
+    width: .4rem;
+    color: $bgColor;
+    .item {
+        text-align: center;
+        line-height: .4rem;
+    }
+}
+</style>
