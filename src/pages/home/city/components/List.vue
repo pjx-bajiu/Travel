30dbdc865cf98772a8ed5930e3199657a5fa1a17
pjx-bajiu <1481877603@qq.com>
2021-12-31 15:49:23 +0800

    Alphabei

--- src/pages/home/city/components/List.vue
+++ src/pages/home/city/components/List.vue
@@ -1,77 +1,56 @@
 <template>
   <div class="list" ref="wrapper">
-   <div>
+    <div>
       <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{ item.name }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area" 
+      v-for="(item,key) of cities" 
+      :key="key" 
+      :ref="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div class="item border-bottom" 
+          v-for="innerItem of item" 
+          :key="innerItem.id">{{innerItem.name}}</div>
         </div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">上海</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">广州</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">深圳</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">杭州</div>
-        </div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">C</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-   </div>
   </div>
 </template>
 
 <script>
-import Bscroll from 'better-scroll'
+import Bscroll from "better-scroll";
 export default {
   name: "CityList",
-  mounted () {
-    this.scroll = new Bscroll(this.$refs.wrapper)
+  props: {
+    hot: Array,
+    cities: Object,
+    letter: String
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.wrapper);
+  },
+  watch: {
+    letter() {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 };
 </script>
@@ -96,12 +75,13 @@ export default {
 }
 
 .list {
-    overflow: hidden;
-    position: absolute;
-    top: 1.58rem;
-    left: 0;
-    right: 0;
-    bottom: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
   .title {
     line-height: 0.54rem;
     background: #eee;
